4644fa54c22e4d318c7ce8aec1f32afafe5de25f
Phay-mann-khaing <phaymann009@gmail.com>
2017-06-21 17:08:27 +0630

    Added main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import convert2csv
 import os
-import openpyxl
+from openpyxl import Workbook
 
 filename = raw_input("File Name: ")
 if os.path.isfile(filename):
     if filename.endswith('.csv'):
         test = convert2csv.Convert2csv()
+        print 'csv file converted to xlsx extension'
     elif filename.endswith('.xlsx'):
-        wb = workbook()
-        pass
+        wb = Workbook()
+        ws = wb.active
+        ws.append(
+            ['line_ids/product_qty', 'line_ids/location_id/id', 'line_ids/product_id/id', 'line_ids/product_uom/id'])
+        wb.save("TestCreate.xlsx")
+        print "Flie created as 'TestCreate.xlsx'"
     else:
         print 'File type invalid'
 else:
