c8931c9d9b3c27d43a5f1b7f42b8be6b428fee30
Phay-mann-khaing <phaymann009@gmail.com>
2017-06-22 16:50:24 +0630

    Fix duplicating the first line

--- main.py
+++ main.py
@@ -13,25 +13,26 @@ if os.path.isfile(filename):
         ws = wb.active
         ws1 = wb1.active
         column_count = ws.max_column
-        row_count = 0
-        max_current_row = 0
+        row_count = 1
+        max_current_row = 2
         a = list(ws.iter_rows())
         for index1, line in enumerate(a, 1):
-            row_count += 1
             for index2, piece in enumerate(line, 1):
-                count = column_count - 3
-                row = 0
-                if row_count == 1:
-                    row = row_count
+                if index1 == 1:
+                    ws1.cell(row=index1, column=index2).value = piece.value
                 else:
-                    row = max_current_row
+                    count = column_count - 3
+                    row = 0
+                    row = row_count
 
-                while count > 0:
-                    ws1.cell(row=row, column=index2).value = piece.value
-                    count -= 1
-                    row += 1
-                if max_current_row < row:
-                    max_current_row = row
+                    while count > 0:
+                        ws1.cell(row=row, column=index2).value = piece.value
+                        count -= 1
+                        row += 1
+                    if max_current_row < row:
+                        max_current_row = row
+
+            row_count = max_current_row
 
         # col_a = ws['A']  # 0-indexing
         # for idx, cell in enumerate(col_a, 1):
