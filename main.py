6bca7440e13c3fd830775bd7fedfd655b8006fa5
Phay-mann-khaing <phaymann009@gmail.com>
2017-06-22 14:09:11 +0630

    Added lines of code that read row of excel

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ if os.path.isfile(filename):
         wb1 = openpyxl.Workbook()
         ws = wb.active
         ws1 = wb1.active
+        column_count = ws.max_column - 3
+        row_count = ws.max_row
+
+        a = list(ws.iter_rows())
+        for index1, line in enumerate(a):
+            for index2, piece in enumerate(line):
+                print piece.value
 
         col_a = ws['A']  # 0-indexing
         for idx, cell in enumerate(col_a, 1):
@@ -23,6 +30,9 @@ if os.path.isfile(filename):
 
         # ws1.append(
         #     ['line_ids/product_qty', 'line_ids/location_id/id', 'line_ids/product_id/id', 'line_ids/product_uom/id'])
+
+        ws1['A1'] = 'line_ids/product_id/id'
+
         wb1.save("/home/phay/PycharmProjects/TestCreate.xlsx")
         print "Flie created as 'TestCreate.xlsx'"
     else:
